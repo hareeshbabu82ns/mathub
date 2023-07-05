0042cc25a47e65a1fde363a7215791545b653880
Hareesh Polla <hareeshbabu82@gmail.com>
2023-07-04 19:06:47 -0600

    ui package updates

--- client/src/pages/ArithmeticTest/SettingsPage.js
+++ client/src/pages/ArithmeticTest/SettingsPage.js
@@ -1,87 +1,90 @@
-import React, { useEffect, useState } from "react";
-import { Button, Grid, IconButton, Stack } from "@mui/material";
+import React, { useEffect, useState } from 'react';
+import { Button, Grid, IconButton, Stack } from '@mui/material';
 import RefreshIcon from '@mui/icons-material/Refresh';
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-import { useQuery, useMutation, gql } from "@apollo/client";
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { useQuery, useMutation, gql } from '@apollo/client';
 
-import { ARITHMETIC_OPERATIONS_ALL, ARITHMETIC_OPERATIONS_DEFAULT } from "../../utils/gen_arithmetic_qa";
-import { FormInputCheckbox } from "../../components/FormInput/FormInputCheckbox";
-import { FormInputSlider } from "../../components/FormInput/FormInputSlider";
-import Panel from "../../components/Panel";
+import {
+  ARITHMETIC_OPERATIONS_ALL,
+  ARITHMETIC_OPERATIONS_DEFAULT,
+} from '../../utils/gen_arithmetic_qa';
+import { FormInputCheckbox } from '../../components/FormInput/FormInputCheckbox';
+import { FormInputSlider } from '../../components/FormInput/FormInputSlider';
+import Panel from '../../components/Panel';
 
-import { FormInputMultiSelect } from "../../components/FormInput/FormInputMultiSelect";
-import _ from "lodash";
-
+import { FormInputMultiSelect } from '../../components/FormInput/FormInputMultiSelect';
+import _ from 'lodash';
 
 const FETCH_TEST_SETTINGS = gql`
-query {
-  testsettings(where: {type: ARITHMETIC}){
-    id
-    settings
+  query {
+    testsettings(where: { type: ARITHMETIC }) {
+      id
+      settings
+    }
   }
-}
-`
+`;
 
 const CREATE_TEST_SETTINGS = gql`
-mutation($settings: JSON) {
-  createTestsetting(
-    data: { type: ARITHMETIC, settings: $settings }
-  ) {
-    id
+  mutation ($settings: Json) {
+    createTestsetting(data: { type: ARITHMETIC, settings: $settings }) {
+      id
+    }
   }
-}
-`
+`;
 
 const UPDATE_TEST_SETTINGS = gql`
-mutation($id:ID!, $settings: JSON) {
-  updateTestsetting(
-    where: { id: $id }
-    data: { type: ARITHMETIC, settings: $settings }
-  ) {
-    id
+  mutation ($id: ID!, $settings: Json) {
+    updateTestsetting(where: { id: $id }, data: { type: ARITHMETIC, settings: $settings }) {
+      id
+    }
   }
-}
-`
+`;
 
 const SettingsPage = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const [count, setCount] = useState(0)
-  const [settings, setSettings] = useState({ ...defaultSettings })
+  const [count, setCount] = useState(0);
+  const [settings, setSettings] = useState({ ...defaultSettings });
 
-  const { loading, error, data, refetch } = useQuery(FETCH_TEST_SETTINGS)
-  const [createTestSetting] = useMutation(CREATE_TEST_SETTINGS)
-  const [updateTestSetting] = useMutation(UPDATE_TEST_SETTINGS)
+  const { loading, error, data, refetch } = useQuery(FETCH_TEST_SETTINGS);
+  const [createTestSetting] = useMutation(CREATE_TEST_SETTINGS);
+  const [updateTestSetting] = useMutation(UPDATE_TEST_SETTINGS);
 
   const onSubmit = async (formData) => {
     // console.log(formData)
     if (!_.isEmpty(settings.id)) {
       // update settings
-      await updateTestSetting({ variables: { id: settings.id, settings: formData } })
+      await updateTestSetting({ variables: { id: settings.id, settings: formData } });
     } else {
-      await createTestSetting({ variables: { settings: formData } })
+      await createTestSetting({ variables: { settings: formData } });
     }
-    navigate('new', { state: { qSettings: { ...formData, max: formData.totalQuestions }, replace: true } })
-  }
+    navigate('new', {
+      state: { qSettings: { ...formData, max: formData.totalQuestions }, replace: true },
+    });
+  };
 
   useEffect(() => {
     if (data?.testsettings?.length >= 1) {
-      const settingData = data.testsettings[0]
+      const settingData = data.testsettings[0];
       // console.log(settingData)
-      setSettings({ ...settingData?.settings, id: settingData.id })
+      setSettings({ ...settingData?.settings, id: settingData.id });
     }
-  }, [data])
+  }, [data]);
 
   return (
     <SettingsView
       key={(settings.id || 'testSettings') + count}
       onSubmit={onSubmit}
-      onRefetch={async () => { await refetch(); setCount(count + 1); }}
+      onRefetch={async () => {
+        await refetch();
+        setCount(count + 1);
+      }}
       loading={loading}
-      defaultValues={settings} />
+      defaultValues={settings}
+    />
   );
 };
 
@@ -93,7 +96,7 @@ export const defaultSettings = {
   timeLimit: 30, //in min
   timeLimitPerQuestion: 30, // in sec
   includingOperations: ARITHMETIC_OPERATIONS_DEFAULT,
-}
+};
 
 const settingsFormSchema = yup.object().shape({
   totalQuestions: yup.number().min(10).max(50).required(),
@@ -105,53 +108,52 @@ const settingsFormSchema = yup.object().shape({
   includingOperations: yup.array(yup.string()).min(1),
 });
 
-const ARITHMETIC_OPERATION_OPTIONS = ARITHMETIC_OPERATIONS_ALL.map(o => ({ label: o, value: o }))
+const ARITHMETIC_OPERATION_OPTIONS = ARITHMETIC_OPERATIONS_ALL.map((o) => ({ label: o, value: o }));
 
 const SettingsView = ({ onSubmit, defaultValues, loading, onRefetch }) => {
-
   const form = useForm({
-    mode: "onSubmit",
+    mode: 'onSubmit',
     defaultValues,
-    resolver: yupResolver(settingsFormSchema)
-  })
-  const { handleSubmit, reset } = form
+    resolver: yupResolver(settingsFormSchema),
+  });
+  const { handleSubmit, reset } = form;
 
   const handleSubmitAPI = async (data) => {
     await onSubmit(data)
       .then(() => form.reset(data))
-      .catch((err) => console.error(err))
+      .catch((err) => console.error(err));
   };
 
   const actionsRight = (
-    <Stack direction="row" spacing={2} >
-      <Button variant={"contained"}
-        onClick={handleSubmit(handleSubmitAPI)} >
+    <Stack direction="row" spacing={2}>
+      <Button variant={'contained'} onClick={handleSubmit(handleSubmitAPI)}>
         Start Test
       </Button>
-      <Button onClick={() => reset()} variant={"outlined"}>
+      <Button onClick={() => reset()} variant={'outlined'}>
         Reset
       </Button>
     </Stack>
-  )
+  );
 
   const toolbarActions = (
     <React.Fragment>
-      <IconButton disabled={loading}
-        onClick={onRefetch}>
+      <IconButton disabled={loading} onClick={onRefetch}>
         <RefreshIcon />
       </IconButton>
     </React.Fragment>
-  )
+  );
 
   return (
-    <Panel title='Settings'
+    <Panel
+      title="Settings"
       toolbarActions={toolbarActions}
       actionsRight={actionsRight}
-      loading={loading}>
+      loading={loading}
+    >
       <SettingsFormView form={form} onSubmit={handleSubmitAPI} />
     </Panel>
-  )
-}
+  );
+};
 
 const SettingsFormView = ({ form, onSubmit }) => {
   const { formState, control, handleSubmit, setValue } = form;
@@ -160,55 +162,71 @@ const SettingsFormView = ({ form, onSubmit }) => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <Grid container spacing={2}>
-
         <Grid item xs={12} sm={6}>
           <FormInputSlider
-            name="totalQuestions" label="Total Questions"
-            control={control} setValue={setValue}
-            min={10} max={50} step={5} />
-        </Grid>
-
-        <Grid item xs={12} sm={6}>
-          <FormInputSlider
-            name="maxDigits" label="Operand Digits"
-            control={control} setValue={setValue}
-            min={1} max={5} />
-        </Grid>
-
-        <Grid item xs={12} sm={6}>
-          <FormInputCheckbox name="nonNegative" label="No -VEs"
-            control={control} />
-        </Grid>
-
-        <Grid item xs={12} sm={6}>
-          <FormInputCheckbox name="nonReminder" label="No Reminder Division"
-            control={control} />
+            name="totalQuestions"
+            label="Total Questions"
+            control={control}
+            setValue={setValue}
+            min={10}
+            max={50}
+            step={5}
+          />
         </Grid>
 
         <Grid item xs={12} sm={6}>
           <FormInputSlider
-            name="timeLimit" label="Time for Test (in Min)"
-            control={control} setValue={setValue}
-            min={0} max={300} step={15} />
+            name="maxDigits"
+            label="Operand Digits"
+            control={control}
+            setValue={setValue}
+            min={1}
+            max={5}
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <FormInputCheckbox name="nonNegative" label="No -VEs" control={control} />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <FormInputCheckbox name="nonReminder" label="No Reminder Division" control={control} />
         </Grid>
 
         <Grid item xs={12} sm={6}>
           <FormInputSlider
-            name="timeLimitPerQuestion" label="Time Per Question (in Sec)"
-            control={control} setValue={setValue}
-            min={0} max={300} step={15} />
+            name="timeLimit"
+            label="Time for Test (in Min)"
+            control={control}
+            setValue={setValue}
+            min={0}
+            max={300}
+            step={15}
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <FormInputSlider
+            name="timeLimitPerQuestion"
+            label="Time Per Question (in Sec)"
+            control={control}
+            setValue={setValue}
+            min={0}
+            max={300}
+            step={15}
+          />
         </Grid>
 
         <Grid item xs={12} sm={6}>
           <FormInputMultiSelect
-            name="includingOperations" label="Arithmetic Operations"
+            name="includingOperations"
+            label="Arithmetic Operations"
             control={control}
             options={ARITHMETIC_OPERATION_OPTIONS}
           />
         </Grid>
-
       </Grid>
     </form>
-  )
-}
-export default SettingsPage
+  );
+};
+export default SettingsPage;
